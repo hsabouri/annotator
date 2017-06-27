741c8a9d71785633c157fcb3b1b91ce8d4ce5153
Etienne ROBERT <etrobert@e2r11p8.42.fr>
2017-06-28 00:30:37 +0200

    crosses improved

--- js/cross.js
+++ js/cross.js
@@ -6,14 +6,14 @@
 //   By: etrobert <etrobert@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2017/06/28 00:16:09 by etrobert          #+#    #+#             //
-//   Updated: 2017/06/28 00:16:11 by etrobert         ###   ########.fr       //
+//   Updated: 2017/06/28 00:29:59 by etrobert         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 function print_line(context, x1, y1, x2, y2)
 {
-	context.moveTo(x1, y1);
-	context.lineTo(x2, y2);
+	context.moveTo(x1 + 0.5, y1 + 0.5);
+	context.lineTo(x2 + 0.5, y2 + 0.5);
 }
 
 function	print_cross(context, x, y, w)
@@ -25,12 +25,16 @@ function	print_cross(context, x, y, w)
 
 function print_fancy_cross(context, x, y)
 {
-	var w = 10;
+	var cross_size = 10;
+	var color = "black";
+	var border = "white";
+	var border_width = "3";
+	var cross_width = "1";
 	
-	context.lineWidth = "4";
-	context.strokeStyle = "gold";
-	print_cross(context, x, y, w);
-	context.lineWidth = "1";
-	context.strokeStyle = "black";
-	print_cross(context, x, y, w);
+	context.lineWidth = border_width;
+	context.strokeStyle = border;
+	print_cross(context, x, y, cross_size);
+	context.lineWidth = cross_width;
+	context.strokeStyle = color;
+	print_cross(context, x, y, cross_size);
 }
