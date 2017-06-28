215dfdf48ff67269e27e4be6db2fdb1f48c4c0a9
hsabouri <hsabouri@student.42.fr>
2017-06-28 03:23:50 +0200

    merge

--- js/main.js
+++ js/main.js
@@ -10,30 +10,58 @@
 //                                                                            //
 // ************************************************************************** //
 
+function image_to_canvas(coord, image) {
+	var ret = {};
+
+	ret.x = coord.x * image.scale;
+	ret.y = coord.y * image.scale;
+
+	return (ret);
+}
+
+function canvas_to_image(coord, image) {
+	var ret = {};
+
+	ret.x = coord.x / image.scale;
+	ret.y = coord.y / image.scale;
+
+	return (ret);
+}
+
 function main()
-{
-	var images = new Images();
+{	
 	var input_files = document.getElementById("input_files");
 	var canvas_element = document.getElementById("canvas");
 	var canvas_manager = new Canvas_manager(canvas_element);
+	var images = new Images();
 
 	input_files.onchange = function (event) {
 		images.save_file_list(event);
 		images.load(1, function(image) {
-			canvas_manager.set_canvas_properties(
-				{
-					width: image.width * image.scale,
-					height: image.height * image.scale
-				});
+			canvas_manager.set_canvas_properties({
+				width: image.width * image.scale,
+				height: image.height * image.scale
+			});
 		});
 	};
 
-	var interval = setInterval(
-		function () { canvas_manager.update_canvas(images); },
-	1000/60);
+	var interval = setInterval(function () {
+		canvas_manager.update_canvas(images);
+	}, 1000/60);
 
-	canvas_element.addEventListener('click',
-		function (event) { canvas_manager.canvas_on_click(event.pageX, event.pageY, images) }, false);
+	canvas_element.addEventListener('click', function (event) {
+		canvas_manager.canvas_on_click(event.pageX, event.pageY, images)
+	}, false);
+
+	document.addEventListener('keydown', function(event) {
+		if (event.key == "ArrowLeft") {
+			images.change_image(-1);
+		}
+
+		if (event.key == "ArrowRight") {
+			images.change_image(1);
+		}
+	});
 }
 
 window.onload = main;
