1dd197baac70d1e124afcff2090daec627582b4e
hsabouri <hsabouri@student.42.fr>
2017-06-28 03:50:59 +0200

    css and save

--- js/main.js
+++ js/main.js
@@ -32,12 +32,15 @@ function main()
 {	
 	var input_files = document.getElementById("input_files");
 	var canvas_element = document.getElementById("canvas");
+	var save_element = document.getElementById("save_json");
+	var filename_element = document.getElementById("filename");
 	var canvas_manager = new Canvas_manager(canvas_element);
 	var images = new Images();
 
 	input_files.onchange = function (event) {
+		input_files.style.display = "none";
 		images.save_file_list(event);
-		images.load(1, function(image) {
+		images.load(0, function(image) {
 			canvas_manager.set_canvas_properties({
 				width: image.width * image.scale,
 				height: image.height * image.scale
@@ -51,6 +54,10 @@ function main()
 
 	canvas_element.addEventListener('click', function (event) {
 		canvas_manager.canvas_on_click(event.pageX, event.pageY, images)
+	}, false);
+
+	save_element.addEventListener('click', function (event) {
+		save_images(images, filename_element.value + ".json");
 	}, false);
 
 	document.addEventListener('keydown', function(event) {
